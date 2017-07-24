4589ddfe156b1a5e303d25c0b9dac1668bad843f
Ramanan Thiyagarajan <rthiyagarajan@users.noreply.github.com>
2017-07-25 06:59:02 +1000

    Corrected build_part2_RNN

--- my_answers.py
+++ my_answers.py
@@ -59,8 +59,9 @@ def window_transform_text(text, window_size, step_size):
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
+    from keras.layers import Activation
     model = Sequential()
     model.add(LSTM(200, input_shape=(window_size,num_chars)))
     model.add(Dense(num_chars, activation='linear'))
-    model.add(Dense(num_chars,activation='softmax'))
+    model.add(Activation('softmax'))
     return model
